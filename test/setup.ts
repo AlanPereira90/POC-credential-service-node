73a8bc6d09e3caaa43e2cc3c11427dd6341ec573
Alan Pereira <alan.pereira@vizir.com.br>
2022-04-08 11:09:16 -0300

    fix: test setup order

--- test/setup.ts
+++ test/setup.ts
@@ -3,10 +3,10 @@ import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
 
+import './setupEnvVars';
+
 import '../src/domain/config/di';
 import '../src/application/config/di';
-
-import './setupEnvVars';
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
